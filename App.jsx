625afce5536dece3788188a2940834e0cad4a633
kelvinogholi-code <kelvinogholi@gmail.com>
2025-09-06 22:06:12 +0100

    Render CultureBridge landing page as main app component.

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CultureBridge from "./culture_bridge_free_website_landing_page.jsx";
+
+function App() {
+  return (
+    <CultureBridge />
+  );
+}
+
+export default App;
